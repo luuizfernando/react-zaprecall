eab95c0c92fa161a28d83380e63e58829cb44b51
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-09-16 13:41:11 -0300

    inserido styled-components

--- zaprecall-react/src/App.jsx
+++ zaprecall-react/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import DeckScreen from "./components/DeckScreen";
+import styled from "styled-components";
 
 
 export default function App() {
