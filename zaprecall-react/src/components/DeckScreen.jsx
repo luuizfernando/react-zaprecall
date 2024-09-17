33d1020eec0f6a4ea73b61c2da0a616e553da700
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-09-17 14:20:34 -0300

    Todos os cards foram criados

--- zaprecall-react/src/components/DeckScreen.jsx
+++ zaprecall-react/src/components/DeckScreen.jsx
@@ -1,19 +1,29 @@
 import React from "react";
+import { useState } from "react";
 
 import Logo from '../assets/logo.png'
 import FlashCard from "./FlashCard";
 import Footer from "./Footer"
 import styled from "styled-components";
+import deckReact from "../constants/deckReact";
 
 export default function DeckScreen() {
 
     return (
 
         <MainContainer>
-            <LogoSite src={Logo} alt="Logo do ZapRecall"/>
-            <h1>ZapRecall</h1>
-            <FlashCard />
-            <Footer />
+            <ContentContainer>
+                <ContainerLogo>
+                    <img src={Logo} alt="Logo do ZapRecall"/>
+                    <h1>ZapRecall</h1>
+                </ContainerLogo>
+                
+                {deckReact.map((card) => 
+                    <FlashCard card={card}/>
+                )}
+
+                <Footer />
+            </ContentContainer>
         </MainContainer>
 
     )
@@ -21,16 +31,31 @@ export default function DeckScreen() {
 }
 
 const MainContainer = styled.div`
-    height: 100vh;
-    width: 100vw;
     display: flex;
     justify-content: center;
-    flex-direction: column;
-    align-items: center;
     background-color: #FB6B6B;
+    height: 120vh;
 `
-
-const LogoSite = styled.img`
-    height: 10vw;
-    width: 10vw;
+const ContentContainer = styled.div`
+    width: 30vw;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`
+const ContainerLogo = styled.div`
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    img {
+        height: 10vw;
+        width: 10vw;
+    }
+    h1 {
+        color: white;
+        font-weight: bold;
+        font-size: 60px;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        margin-left: 50px;
+    }
 `
--- zaprecall-react/src/components/FlashCard.jsx
+++ zaprecall-react/src/components/FlashCard.jsx
@@ -1,7 +1,101 @@
-export default function FlashCard() {
+import { useState } from "react";
+import styled from "styled-components"
+import SetaPlay from "../assets/seta_play.png"
+import SetaVirar from "../assets/seta_virar.png"
+
+export default function FlashCard({ card }) {
+
+    const [started, setStarted] = useState(false);
+    const [turned, setTurned] = useState(false);
+    const [finished, setFinished] = useState(false);
+
+    function showQuestion() {
+
+    }
+
+    function showAnswer() {
+        
+    }
+
+    function answerCard() {
+        
+    }
+
     return (
-        <div>
-            FlashCard
-        </div>
+        <CardInicial>
+            <p>Pergunta {card.id}</p>
+            <img src={SetaPlay} alt="" onClick={showQuestion}/>
+        </CardInicial>
+        // <CardPergunta>
+        //     <p>{card.question}</p>
+        //     <img src={SetaVirar} alt="" onClick={showAnswer}/>
+        // </CardPergunta>
+        // <CardResposta>
+        //     <p>{card.answer}</p>
+        //     <ContainerButtons>
+        //         <button>Não lembrei</button>
+        //         <button>Quase não lembrei</button>
+        //         <button>Zap!</button>
+        //     </ContainerButtons>
+        // </CardResposta>
     )
 }
+
+const CardInicial = styled.div`
+    padding: 20px 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: white;
+    border-radius: 7px;
+    font-size: 16px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    img {
+        cursor: pointer;
+    }
+`
+const CardPergunta = styled.div`
+    padding: 45px 15px;
+    display: flex;
+    justify-content: space-between;
+    background-color: #FFFFD4;
+    border-radius: 7px;
+    font-size: 16px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+`
+const CardResposta = styled.div`
+    padding: 20px 15px;
+    display: flex;
+    flex-direction: column;
+    background-color: #FFFFD4;
+    border-radius: 7px;
+    font-size: 16px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    p {
+        text-align: center;
+    }
+`
+const ContainerButtons = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 30px;
+    button {
+        border: 0;
+        border-radius: 10px;
+        width: 30%;
+        height: 40px;
+        color: white;
+        font-size: 16px;
+    }
+    button:first-child {
+        background-color: #FF3030;
+    }
+    button:nth-child(2) {
+        background-color: #FF922E;
+    }
+    button:nth-child(3) {
+        background-color: #2FBE34;
+    }
+`
